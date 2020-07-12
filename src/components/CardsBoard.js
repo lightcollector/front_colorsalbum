65d58f3154944f7d97b8bd83c4525dbc963bba48
Contenidos Bexfy <contenidosbexfy@MacBook-Pro-de-Contenidos-4.local>
2020-07-12 13:33:01 +0200

    Escritura del elemento copiado en el clipboard y gestión de la notificacion de éxito

--- src/components/CardsBoard.js
+++ src/components/CardsBoard.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ColorCard from './ColorCard'
 import { Row } from 'react-bootstrap';
+
+
+
 const CardsBoards = ({ colorList }) => {
+  const [colorCopied, toggleCopy] = useState(false);
+  const [colorID, setColorID] = useState('');
+  
+  // hook for 
+  useEffect(() => {
+    console.log('effect runs');
+    const interval = setInterval(() => console.log("copiadoState: " + colorCopied), 1000);
+
+    if(colorCopied) {
+      setTimeout(() => toggleCopy(false), 3000);
+    }
+
+    return () => clearInterval(interval);
+  }, [colorCopied]);
+
+
+  /**
+  * Function to change hook state.
+  * - Triggered on clipboard-write succeess in child.
+  * - Updates de ID of the color copied, and sets a flag to display de success notification.
+  * @param  {int}
+  */ 
+  function copySuccess(colorID) {
+    setColorID(colorID);
+    toggleCopy(true);
+    console.log("colorID: " + colorID)
+  }
+
   return (
-    <Row>
+    <Row className="cardsBoard">
       {colorList.map((color, i) => (
-          <ColorCard className="cardsBoard" key={i} props={color} />
+          <ColorCard key={i} props={color} copyColor={copySuccess} />
       ))}
     </Row>
   )
--- src/components/ColorCard.js
+++ src/components/ColorCard.js
@@ -2,15 +2,35 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 
 const ColorCard = (props) => {
-  
-  var checkData = () =>{
-    console.log("la info");
-    console.log (props);
+/**
+ * Function to write a string into the clipboard
+ * - Takes the string value to write, and also the color ID and a function (from parent)
+ * - to call when clipboard-write is succeeded
+ * @param  {string, int, function}
+ */ 
+  function updateClipboard(newClip, colorId, copySuccessFn) {
+    console.log("Le has dado lick a: " + newClip);
+    // checking Permissions API's "clipboard-write" permission
+    navigator.permissions.query({name: "clipboard-write"}).then(result => {
+      if (result.state === "granted" || result.state === "prompt") {
+        // if browser allows clipboard-write
+        navigator.clipboard.writeText(newClip).then(function() {
+          // clipboard successfully set
+          // call parent function for display "copied!" state
+          copySuccessFn(colorId);
+        }, function() {
+          // clipboard write failed
+          alert("El color no se ha podido copiar correctamente")
+        });
+      } else {
+        alert("Tu navegador no sopoorta copiado automático al clicar los colores")
+      }
+    });
   }
-  
+
   return (
-    <Col xs={6} sm={6} md={4} lg={4}
-      onClick={checkData()} className="colorCard"  
+    <Col xs={6} sm={6} md={4} lg={4} className="colorCard"  
+      onClick={() => updateClipboard(props.props.color, props.props.id, props.copyColor)}      
       style={{backgroundColor : `${props.props.color}`}}>
       
       <p className="colorYear">{props.props.year}</p>
